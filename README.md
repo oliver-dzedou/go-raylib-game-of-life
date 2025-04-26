8512477cb0588baf8f3cc085ef0434a100a2b8d3
Oliver Dzedou <sperlik.oliver@outlook.com>
2025-04-26 17:16:12 +0000

    fix naming

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Sandboxiest Sandbox 
+# Go Raylib Game Of Life
+
+Conway's Game of Life implemented in Go & Raylib
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-rm -r ./build || mkdir ./build && CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 go build -x -o ./build/Sandboxiest\ Sandbox.exe -ldflags "-s -w" && go build -x -o ./build/Sandboxiest\ Sandbox .
+rm -r ./build || mkdir ./build && CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 go build -x -o ./build/gameoflife.exe -ldflags "-s -w" && go build -x -o ./build/gameoflife .
 
 
 
